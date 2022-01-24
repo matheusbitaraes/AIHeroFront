5a740fc93623b184ebc6d2560f220115d81fd59c
matheus <matheusbitaraesdenovaes@gmail.com>
2022-01-23 23:10:52 -0300

    Initial WIP website

--- src/App.js
+++ src/App.js
@@ -1,163 +1,45 @@
-import './App.css';
-import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import React, { useRef, useEffect } from 'react';
+import { useLocation, Switch } from 'react-router-dom';
+import AppRoute from './utils/AppRoute';
+import ScrollReveal from './utils/ScrollReveal';
+import ReactGA from 'react-ga';
 
-const AI_HERO_URL = "http://localhost:5000"
-const TRAIN_PATH = "train/melodies"
-const GAN_PATH = "gan/melodies"
-const MELODY_PATH = "melodies"
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
 
+// Views 
+import Home from './views/Home';
 
-function App() {
-  const [input, setInput] = useState(JSON.stringify([
-    {
-      "melodic_part": "RELAXATION",
-      "chord": "C",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "TENSION",
-      "chord": "F",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "RELAXATION",
-      "chord": "C",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "RELAXATION",
-      "chord": "C",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "RELAXATION",
-      "chord": "F",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "TENSION",
-      "chord": "F",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "RELAXATION",
-      "chord": "C",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "RELAXATION",
-      "chord": "C",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "TENSION",
-      "chord": "G",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "TENSION",
-      "chord": "F",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "RELAXATION",
-      "chord": "C",
-      "key": "C",
-      "tempo": 120
-    },
-    {
-      "melodic_part": "RETAKE",
-      "chord": "G",
-      "key": "C",
-      "tempo": 120
-    }
-  ]
-  ));
-  const [postResult, setPostResult] = useState(null);
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
 
-  async function getMelody(type) {
-    let path = "/"
-    switch (type) {
-      case "train":
-        path = TRAIN_PATH
-        break;
-      case "gan":
-        path = GAN_PATH
-        break;
-      default:
-        path = MELODY_PATH
-        break;
-    }
+const trackPage = page => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
 
-    try {
-      const res = await fetch(`${AI_HERO_URL}/${path}`, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          "x-access-token": "token-value"
-        },
-        body: input,
-      });
+const App = () => {
 
-      if (!res.ok) {
-        const message = `An error has occured: ${res.status} - ${res.statusText}`;
-        throw new Error(message);
-      }
+  const childRef = useRef();
+  let location = useLocation();
 
-      const data = await res.json();
+  useEffect(() => {
+    const page = location.pathname;
+    document.body.classList.add('is-loaded')
+    childRef.current.init();
+    trackPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
 
-      const result = {
-        status: res.status + "-" + res.statusText,
-        headers: {
-          "Content-Type": res.headers.get("Content-Type"),
-          "Content-Length": res.headers.get("Content-Length"),
-        },
-        data: data,
-      };
-
-      setPostResult(result);
-    } catch (err) {
-      setPostResult(err.message);
-    }
-
-    console.log(postResult)
-  }
-
-return (
-  <div className="App">
-    <header className="App-header">
-    </header>
-    <body>
-      <div className="input-container">
-        <TextField id="outlined-basic"
-          multiline
-          rows={20}
-          // onChange={(v,a)=>setInput(JSON.parse(a))}
-          value={input}
-          label="Melody Specs (JSON)" variant="outlined" />
-      </div>
-      <div className="button-container">
-        <Button className="input-button" id="from-train" variant="contained" color="primary" onClick={() => getMelody("train")}>Generate from train data</Button>
-        <Button className="input-button" id="from-gan" variant="contained" color="primary" onClick={() => getMelody("gan")}>Generate from GAN data</Button>
-        <Button className="input-button" id="from-all" variant="contained" color="primary" onClick={() => getMelody()}>Generate melody</Button>
-      </div>
-    </body>
-
-
-  </div>
-);
+  return (
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Switch>
+          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+        </Switch>
+      )} />
+  );
 }
 
 export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import sections
+import Hero from '../components/sections/Hero';
+
+const Home = () => {
+
+  return (
+    <>
+      <Hero className="illustration-section-01" />
+      {/* <FeaturesTiles />
+      <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
+      <Testimonial topDivider />
+      <Cta split /> */}
+    </>
+  );
+}
+
+export default Home;
