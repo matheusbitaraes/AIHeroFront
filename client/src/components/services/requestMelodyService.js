9351cc243922226d58c5b8c6e5bc51fee1c67f91
matheus <matheusbitaraesdenovaes@gmail.com>
2023-04-03 15:28:57 -0300

    fix services errors

--- client/src/components/services/requestMelodyService.js
+++ client/src/components/services/requestMelodyService.js
@@ -5,18 +5,35 @@ const {host, port} = config.server
 const url =  `${host}:${port}`
 
 
-export default async function pollForMelodyService ({melodyId}) {
+export default async function requestMelodyService ({harmonySpecs, evolutionarySpecs, melodyFrom}) {
 
+  try {
     const requestOptions = {
-        method: "GET",
-        responseType: 'blob'
-      };
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*", //todo: improve
+        "Access-Control-Allow-Credentials": "true",
+      },
+      body: JSON.stringify({
+        harmony_specs: harmonySpecs,
+        evolutionary_specs: evolutionarySpecs,
+      }),
+    };
 
-      const response = await fetch(`${url}/api/melody/${melodyId}`, requestOptions)
-      
-      if (response.ok) {
-        return response.blob()
+    const response = await fetch(`${url}/api/melody/${melodyFrom}`, requestOptions)
+
+    const body = await response.json();
+
+    const { melodyId, melodyUrl} = body.data
+
+    if (!melodyId) {
+        throw new Error('Missing melodyId');
       }
 
-      return null
+    return {melodyId, melodyUrl}
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
 }
--- server/services/requestMelodyGeneration.js
+++ server/services/requestMelodyGeneration.js
@@ -1,12 +1,14 @@
 const axios = require('axios');
+const config = require('../config')
+
+const {dns, port} = config.aiherogenerator
+
 
 const requestMelodyGeneration = async (req, res) => {
     const { params, body } = req
     const { source } = params
 
-    const endpoint = 'http://localhost:8083' // 'http://ai-hero-service:8083'
-
-    const url = `${endpoint}/melody?source=${source}`
+    const url = `${dns}:${port}/melody?source=${source}`
 
     const response = await axios.post(url, body)
 
